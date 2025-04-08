bbef66d6d15cb57ed3abf03350f50467c0036744
Manas Bhalerao <manasbhalerao1@gmail.com>
2025-04-08 13:39:42 +0530

    BE url

--- src/atoms/contentAtom.tsx
+++ src/atoms/contentAtom.tsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { atom, atomFamily, selectorFamily } from "recoil";
-import { BACKEND_URL } from "../config";
 import { contentdata } from "../types";
 
 export const contentAtom = atomFamily({
@@ -13,7 +12,7 @@ export const contentAtom = atomFamily({
         key:"contentselector",
         get: (id :string | null) => async() => {
             if(!id){
-                const response = await axios.get(`${BACKEND_URL}/api/v1/content`,
+                const response = await axios.get(`${import.meta.env.BACKEND_URL}/api/v1/content`,
                     {
                         withCredentials: true
                     }
@@ -22,7 +21,7 @@ export const contentAtom = atomFamily({
             }
             else{
                 //check point for hitting BE
-                const response = await axios.post(`${BACKEND_URL}/brain/${id}`,
+                const response = await axios.post(`${import.meta.env.BACKEND_URL}/brain/${id}`,
                     // {
                     //     withCredentials: true
                     // }
